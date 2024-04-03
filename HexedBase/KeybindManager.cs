e489945025e9201ecd0481753ab355f2e265eef3
Umbra <69671761+Umbra999@users.noreply.github.com>
2024-04-03 02:38:53 +0200

    Improvements

--- HexedBase/KeybindManager.cs
+++ HexedBase/KeybindManager.cs
@@ -4,18 +4,20 @@ namespace HexedBase
 {
     internal class KeybindManager
     {
+        private static float ogFOV = 60;
         private static bool isCameraZoomed = false;
         public static void Update()
         {
             if (Input.GetKeyInt(KeyCode.LeftAlt) && !isCameraZoomed && !Input.GetKeyInt(KeyCode.Tab))
             {
+                ogFOV = Camera.main.fieldOfView;
                 isCameraZoomed = true;
                 Camera.main.fieldOfView = 10;
             }
             else if (Input.GetKeyUpInt(KeyCode.LeftAlt) && isCameraZoomed || !Application.isFocused)
             {
                 isCameraZoomed = false;
-                Camera.main.fieldOfView = 60;
+                Camera.main.fieldOfView = ogFOV;
             }
         }
     }
--- HexedBase/PatchManager.cs
+++ HexedBase/PatchManager.cs
@@ -1,6 +1,5 @@
 ﻿using CoreRuntime.Manager;
 using System;
-using VRC.SDKBase;
 
 namespace HexedBase
 {
@@ -9,10 +8,10 @@ namespace HexedBase
         private delegate void _OnNetworkReadyDelegate(IntPtr instance);
         private static _OnNetworkReadyDelegate originalMethod;
 
-        // Create a hook using the HookManager, if you need to know more about hooking read it up on the internet since its a complex task i wont explain here so much
+        // Create a hook using the HookManager, if you need to know more about hooking read it up on the internet since its a complex task i wont explain here
         public static void ApplyPatch()
         {
-            originalMethod = HookManager.Detour<_OnNetworkReadyDelegate>(typeof(VRC.Player).GetMethod(nameof(VRC.Player.OnNetworkReady)), Patch);
+            originalMethod = HookManager.Detour<_OnNetworkReadyDelegate>(typeof(VRCPlayer).GetMethod(nameof(VRCPlayer.OnNetworkReady)), Patch);
         }
 
         private static void Patch(IntPtr instance)
@@ -21,7 +20,7 @@ namespace HexedBase
             originalMethod(instance);
 
             // Cast our Pointer to a valid Player like its orginally used
-            VRC.Player player = instance == IntPtr.Zero ? null : new VRC.Player(instance);
+            VRCPlayer player = instance == IntPtr.Zero ? null : new VRCPlayer(instance);
 
             if (player == null) return;
 
